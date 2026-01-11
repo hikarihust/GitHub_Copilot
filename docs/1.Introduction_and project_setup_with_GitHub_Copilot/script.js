a246c8971bfd2f677d2b246965b375ed02cc3352
FPT_QuangVD21 <quangvd21@fpt.com>
2026-01-11 20:43:05 +0900

    GitHub Copilot inline suggestions and next edit suggestions

---
+++ docs/1.Introduction_and project_setup_with_GitHub_Copilot/script.js
@@ -0,0 +1,8 @@
+function getAllLabels(){
+    return document.querySelectorAll('label');
+}
+
+window.onload = function() {
+    // query for all html lable tags
+    const labels = getAllLabels();
+}
